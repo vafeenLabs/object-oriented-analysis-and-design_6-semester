7d466ad32c4e2a2d8a01263865bfbf7a404e7498
Vafeen <666av6@gmail.com>
2025-03-05 11:20:47 +0300

    Add lab3 and lab4 tasks

--- src/main/kotlin/lab4/lab4.kt
+++ src/main/kotlin/lab4/lab4.kt
@@ -3,6 +3,12 @@ package lab4
 import lab4.goblin.GoblinArmyFactory
 import lab4.orc.OrcArmyFactory
 
+/**
+ * Паттерн: Абстрактная фабрика
+ * Есть Гоблины и Орки
+ * У каждого войска одинаковая иерархия: Генерал, Главнокомандующий, Солдаты
+ * Реализовано создание войск и война
+ */
 fun main() {
     War(armyFactory1 = GoblinArmyFactory(), armyFactory2 = OrcArmyFactory()).go()
 }
